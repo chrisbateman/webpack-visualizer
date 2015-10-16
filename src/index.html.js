5d18c4997799df6cf29cf34a6995423d74147f1a
Chris Bateman <chrisb808@gmail.com>
2015-10-16 04:58:21 +0000

    removed exp code

--- src/index.html.js
+++ src/index.html.js
@@ -11,46 +11,6 @@ export default function(cfg) {
 </head>
 <body>
     <div id="App">${cfg.appHTML}</div>
-    <script>
-    (function() {
-        var warned = false;
-        var warning;
-        
-        document.addEventListener('click', handleClick);
-        
-        function handleClick(ev) {
-            var nodeName = ev.target.nodeName;
-            if (nodeName === 'a' || nodeName === 'button' || ev.target.getAttribute('role') === 'button') {
-                showWarning();
-                removeClickHandler();
-            }
-        }
-        function removeClickHandler() {
-            document.removeEventListener('click', handleClick);
-        }
-        function showWarning() {
-            warning = document.createElement('div');
-            warning.innerHTML = 'not yet!';
-            warning.style.position = 'fixed';
-            warning.style.background = 'rgba(0,0,0,0.5)';
-            warning.style.left = 0;
-            warning.style.right = 0;
-            warning.style.top = 0;
-            warning.style.bottom = 0;
-            document.body.appendChild(warning);
-            
-            warned = true;
-        }
-        function removeWarning() {
-            document.body.removeChild(warning);
-        }
-        window.appIsReadyNow = function() {
-            removeWarning();
-            removeClickHandler();
-            delete window.appIsReadyNow;
-        }
-    })();
-    </script>
     <script src="build.js"></script>
     <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
